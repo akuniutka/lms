35c82f3801aa71db2a8b69f34d8460a2ef4e9d2e
Andrei Kuniutka <akuniutka@gmail.com>
2023-03-10 20:31:31 +0300

    Added logging.

--- src/main/java/dev/akuniutka/skillfactory/lms/Main.java
+++ src/main/java/dev/akuniutka/skillfactory/lms/Main.java
@@ -3,11 +3,15 @@ package dev.akuniutka.skillfactory.lms;
 import dev.akuniutka.skillfactory.lms.connectors.XLSXConnector;
 import dev.akuniutka.skillfactory.lms.models.Student;
 import dev.akuniutka.skillfactory.lms.models.University;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     private static final String DATA_FILE_NAME = "universityinfo.xlsx";
+
     public static void main(String[] args) {
         try {
             List<University> universities = XLSXConnector.getUniversitiesList(DATA_FILE_NAME);
@@ -25,7 +29,9 @@ public class Main {
                 System.out.println(student);
             }
         } catch (Exception e) {
-            System.err.println("File '" + DATA_FILE_NAME + "' has a wrong format!");
+            LOGGER.error("Error reading file '" + DATA_FILE_NAME + "': file not found or has a wrong format!");
+            LOGGER.error(e.getMessage());
+            System.err.println("File '" + DATA_FILE_NAME + "' not found or has a wrong format!");
         }
     }
 }
--- src/main/java/dev/akuniutka/skillfactory/lms/connectors/XLSXConnector.java
+++ src/main/java/dev/akuniutka/skillfactory/lms/connectors/XLSXConnector.java
@@ -1,5 +1,8 @@
 package dev.akuniutka.skillfactory.lms.connectors;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import dev.akuniutka.skillfactory.lms.enums.StudyProfile;
 import dev.akuniutka.skillfactory.lms.models.*;
 import org.apache.poi.ss.usermodel.Row;
@@ -12,16 +15,21 @@ import java.util.List;
 import java.util.Iterator;
 
 public class XLSXConnector {
+    private static final Logger LOGGER = LoggerFactory.getLogger(XLSXConnector.class);
+
     private XLSXConnector() {}
 
     public static List<Student> getStudentsList(String fileName) throws IOException {
         List<Student> students = new ArrayList<>();
+        LOGGER.debug("Opening XLSX workbook from file '" + fileName + "'");
         XSSFWorkbook workbook = new XSSFWorkbook(fileName);
+        LOGGER.debug("Locating worksheet #0");
         XSSFSheet sheet = workbook.getSheetAt(0);
         Iterator<Row> rows = sheet.iterator();
         // Skip headings row
         if (rows.hasNext()) {
-            rows.next();
+            Row row = rows.next();
+            LOGGER.debug("Skipped row #" + row.getRowNum());
         }
         while (rows.hasNext()) {
             Row row = rows.next();
@@ -30,6 +38,7 @@ public class XLSXConnector {
                     .setFullName(row.getCell(1).getStringCellValue())
                     .setCurrentCourseNumber((int)(row.getCell(2).getNumericCellValue()))
                     .setAvgExamScore((float)(row.getCell(3).getNumericCellValue()));
+            LOGGER.debug("Read row #" + row.getRowNum());
             students.add(student);
         }
         workbook.close();
@@ -38,12 +47,15 @@ public class XLSXConnector {
 
     public static List<University> getUniversitiesList(String fileName) throws IOException {
         List<University> universities = new ArrayList<>();
+        LOGGER.debug("Opening XLSX workbook from file '" + fileName + "'");
         XSSFWorkbook workbook = new XSSFWorkbook(fileName);
+        LOGGER.debug("Locating worksheet #1");
         XSSFSheet sheet = workbook.getSheetAt(1);
         Iterator<Row> rows = sheet.iterator();
         // Skip headings row
         if (rows.hasNext()) {
-            rows.next();
+            Row row = rows.next();
+            LOGGER.debug("Skipped row #" + row.getRowNum());
         }
         while (rows.hasNext()) {
             Row row = rows.next();
@@ -53,6 +65,7 @@ public class XLSXConnector {
                     .setShortName(row.getCell(2).getStringCellValue())
                     .setYearOfFoundation((int)(row.getCell(3).getNumericCellValue()))
                     .setMainProfile(StudyProfile.valueOf(row.getCell(4).getStringCellValue()));
+            LOGGER.debug("Read row #" + row.getRowNum());
             universities.add(university);
         }
         workbook.close();
